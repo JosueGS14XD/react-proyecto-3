51b2955b1c2c58d40222970644324930ce04f560
JosueGS14XD <josuegallegos142@gmail.com>
2025-08-27 04:39:22 -0500

    Primer commit proyecto 3

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+//import"./style.css"
+import styles from "./Card.module.sass"
+
+const Card = () => {
+  return (
+    <div className={styles.Card}>
+        soy una card !
+    </div>
+  )
+
+}
+
+export default Card;
